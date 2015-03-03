6ad91091824d9f9a04e60920af018a0a901488a4
deek2295 <deryk_thuss@hotmail.com>
2015-03-03 13:51:35 -0500

    [FIX] Background to image, scaling

--- opengltest/Menu.cpp
+++ opengltest/Menu.cpp
@@ -5,7 +5,7 @@ MenuClass::MenuClass()
 	background = new Sprite();
 	background->loadSpriteSheet("assets/menu/background.png");
 	background->setNumberOfAnimations(1);
-	background->setSpriteFrameSize(359, 398);
+	background->setSpriteFrameSize(1210, 833);
 	background->addSpriteAnimFrame(0, 0, 0);
 	background->setPosition(0, 0);
 }
@@ -19,6 +19,6 @@ void MenuClass::Draw()
 {
 	for (int i = 0, s = effects.size(); i < s; i++)
 		effects.at(i)->draw(1.f);
-	background->draw(0.5f);
+	background->draw(0.23f);
 }
 
