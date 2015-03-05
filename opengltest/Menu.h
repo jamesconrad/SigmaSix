a21297f1443288031de044c09a6a3d5989f1346b
deek2295 <deryk_thuss@hotmail.com>
2015-03-05 12:50:52 -0500

    [FIX] background.png image on menu

--- opengltest/Menu.h
+++ opengltest/Menu.h
@@ -10,6 +10,9 @@ public:
 	MenuClass();
 	void Draw();
 	void Update(float dTime);
+	//button == GLUT_ABCD_BUTTON
+	//state == GLUT_DOWN || GLUT_UP
+	void MouseClick(int button, int state, int x, int y);
 
 
 private:
