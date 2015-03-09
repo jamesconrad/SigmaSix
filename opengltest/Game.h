f4c69621091ca8eea94d72b5563b9214cea7ebde
jamesconrad <james.conrad@uoit.net>
2015-03-09 18:52:04 -0400

    [WRK] Cosmetics

--- opengltest/Game.h
+++ opengltest/Game.h
@@ -189,5 +189,5 @@ private:
 	ProjectileManager* projectileManager;
 	ColisionManager* colisionManager;
 	MapLoader* mapLoader;
-	Sprite *hpBar, *energyBar, *hpBG, *s_Score, *s_Time;
+	Sprite *hpBar, *energyBar, *hpBG, *s_Score, *s_Time, *entBarSprite;
 };
