e0cfcca3ec8c4c44d4d82788a8d99171a783e6b3
preator33 <tyranid99@hotmail.com.com>
2015-03-11 21:08:14 -0400

    {WRK] Finishing off Aura,

--- opengltest/Aura.cpp
+++ opengltest/Aura.cpp
@@ -17,25 +17,32 @@ void Aura::createProjectiles()
 
 	for (int i = size; i >= -size; i--)
 	{//this way it puts a projectile in every space within the duration 
-		ProjectileManager::instance()->CreateProjectile(66, (locX - i), (locY - i), 0, 0, damage, duration, 0);
-	}
-		// start x & start Y are the bottom left hand corner 
+		for (int j = size; j >= -size; j--)
+		{
+			ProjectileManager::instance()->CreateProjectile(66, (locX - i), (locY - i), 0, 0, damage, ProjDuration, 0);
+		}
+	}	// start x & start Y are the bottom left hand corner 
 		// max dur = aprox 1 
 		//projetile speed = 0 
 		//dir x & y = 0
 		
 }
 
-void Aura::Update(float dTime)
+void Aura::Update()
 {
-
+	
 	size = (owner->getMaxHP() - owner->getHP())*scaler;
-	cooldown -= dTime;
+	
 	createProjectiles();
 }
 
 void Aura::Activate()
 {
-	
+	while (tElapsed < AbilityDuration)
+	{
+		Update();
+			tElapsed--;
+
+	}
 }
 
