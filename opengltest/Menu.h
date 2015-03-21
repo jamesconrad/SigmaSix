2f17fd9dda8801929600d588c37b5eceb0a9f150
deek2295 <deryk_thuss@hotmail.com>
2015-03-21 14:12:50 -0400

    [ADD] Menu stuff

--- opengltest/Menu.h
+++ opengltest/Menu.h
@@ -7,18 +7,18 @@
 class MenuClass
 {
 public:
-	MenuClass();
+	MenuClass(int num);
 	void Draw();
 	void Update(float dTime);
 	//button == GLUT_ABCD_BUTTON
 	//state == GLUT_DOWN || GLUT_UP
 	void MouseClick(int button, int state, int x, int y);
-
+	void ChangeImage(int newImage);
 
 private:
 	std::vector<Sprite*> effects;
 	Sprite* background;
-
+	int selection;
 
 };
 
