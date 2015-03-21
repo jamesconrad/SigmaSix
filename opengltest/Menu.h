dc7e401daa11e85de718594d3f9abccf1e66feb9
jamesconrad <james.conrad@uoit.net>
2015-03-21 14:19:53 -0400

    [ADD] Menu Keypress

--- opengltest/Menu.h
+++ opengltest/Menu.h
@@ -12,13 +12,18 @@ public:
 	void Update(float dTime);
 	//button == GLUT_ABCD_BUTTON
 	//state == GLUT_DOWN || GLUT_UP
-	void MouseClick(int button, int state, int x, int y);
+	void KeyPress(unsigned char key, bool press);
 	void ChangeImage(int newImage);
+
+
+	bool StartGame() { return startGame; }
 
 private:
 	std::vector<Sprite*> effects;
 	Sprite* background;
 	int selection;
+	bool startGame;
+	bool keys[256];
 
 };
 
