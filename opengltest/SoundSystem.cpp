f48814f9e7a09201fb90d81ec5279352c7269e81
jamesconrad <james.conrad@uoit.net>
2015-03-21 19:03:33 -0400

    [ADD] Sound effects

--- opengltest/SoundSystem.cpp
+++ opengltest/SoundSystem.cpp
@@ -5,6 +5,12 @@ SoundSystemClass::SoundSystemClass()
 	//init FMOD
 	FMOD::System_Create(&system);// create an instance of the game engine
 	system->init(32, FMOD_INIT_NORMAL, 0);// initialise the game engine with 32 channels
+
+	FMOD::Sound *sound;
+	system->createSound("assets/sound/plyr_hurt.mp3", FMOD_DEFAULT, NULL, &sound);
+	soundEffects.push_back(sound);
+	system->createSound("assets/sound/plyr_shoot.mp3", FMOD_DEFAULT, NULL, &sound);
+
 }
 
 void SoundSystemClass::PlayBackground(char* fp)
@@ -14,6 +20,7 @@ void SoundSystemClass::PlayBackground(char* fp)
 	system->playSound(bg, NULL, false, &channel);
 }
 
-void PlaySound(char* fp)
+void SoundSystemClass::PlaySound(soundEffect effect)
 {
+	system->playSound(soundEffects.at(effect), NULL, false, &effects);
 }
