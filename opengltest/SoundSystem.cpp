a153878bc0e0f49dc734f3d3bc9afc550614455e
jamesconrad <james.conrad@uoit.net>
2015-03-21 18:50:54 -0400

    [FIX] Sounds stop when entering new level

--- opengltest/SoundSystem.cpp
+++ opengltest/SoundSystem.cpp
@@ -9,6 +9,7 @@ SoundSystemClass::SoundSystemClass()
 
 void SoundSystemClass::PlayBackground(char* fp)
 {
+	channel->stop();
 	system->createSound(fp, FMOD_LOOP_NORMAL, NULL, &bg);
 	system->playSound(bg, NULL, false, &channel);
 }
