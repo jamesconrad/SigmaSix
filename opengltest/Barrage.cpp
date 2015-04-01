d0ac17664429933ac2be608566fec15f8516e402
preator33 <tyranid99@hotmail.com.com>
2015-04-01 14:25:13 -0400

    [wrk] overloaded onhit for barrage

---
+++ opengltest/Barrage.cpp
@@ -0,0 +1,96 @@
+#include "Barrage.h"
+
+
+
+Barrage::Barrage(Entity* own, float cd, float pR, float tSize,  float tduration) : Item(owner, cooldown, procRate)
+{
+	size = tSize;
+	damage = own->getDamage;
+	duration = tduration;
+}
+
+
+
+
+void Barrage:: 
+Update(float dTime)
+{
+	cooldown -= dTime;
+
+	if (cooldown <= 0)
+	{
+		nextShot = true;
+	}
+
+}
+
+
+
+
+void Barrage::
+OnHit(int projectileID)
+{
+	if (nextShot = true)
+	{
+		//the location of impact
+		float x;
+		float y;
+		owner->BarrageLocation(projectileID);
+
+		for (int i = -2; i < 2; i++)
+		{
+			for (int j = -2; j < 2; j++)
+			{
+				ProjectileManager::instance()->CreateProjectile(69, x + i, y + j, 0, 0, damage, duration, 0, owner->GetIndex());
+			}
+		}
+	}
+}
+ 
+
+void Barrage::
+OnHit(vec2 location)
+{
+	if (nextShot = true)
+	{
+	
+		for (int i = -2; i < 2; i++)
+		{
+			for (int j = -2; j < 2; j++)
+			{
+				ProjectileManager::instance()->CreateProjectile(69, location.x + i, location.y + j, 0, 0, damage, duration, 0, owner->GetIndex());
+			}
+		}
+	}
+
+}
+
+
+
+void Barrage::
+OnFire()
+{/*
+	if (nextShot = true)
+	{
+		nextShot = false;
+		cooldown = maxCooldown;
+		float dirX = owner->GetDirection().x;
+		float dirY = owner->GetDirection().y;
+		ProjectileManager::instance()->CreateProjectile(66, (owner->GetLocationX), (owner->getLocationY), dirX, dirY, damage, duration, 0, owner->GetIndex());
+
+	}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- opengltest/Barrage.h
+++ opengltest/Barrage.h
@@ -6,6 +6,7 @@ class Barrage : public Item
 public:
 	Barrage(Entity* own, float cd, float pR, float tSize,  float tduration);
 	void OnHit(int projectileID);
+	void OnHit(vec2 location);
 	void Activate();
 	void Update(float dTime);
 	void OnFire();
